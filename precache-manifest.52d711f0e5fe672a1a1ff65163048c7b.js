01fc04dc9afe8d7733f29eee2ae92f59a8676ae2
damilola5468 <lorlahstrings@gmail.com>
2020-09-24 18:31:16 +0100

    Updates

---
+++ precache-manifest.52d711f0e5fe672a1a1ff65163048c7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df014cfecc8831f2ac0214afa5e69af",
+    "url": "/react-dashboard/index.html"
+  },
+  {
+    "revision": "bec0b72a8c891863ffd7",
+    "url": "/react-dashboard/static/css/2.a333aad8.chunk.css"
+  },
+  {
+    "revision": "dae3cf49fcf8a768171f",
+    "url": "/react-dashboard/static/css/main.9b08047f.chunk.css"
+  },
+  {
+    "revision": "bec0b72a8c891863ffd7",
+    "url": "/react-dashboard/static/js/2.533aa516.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-dashboard/static/js/2.533aa516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae3cf49fcf8a768171f",
+    "url": "/react-dashboard/static/js/main.b25769f1.chunk.js"
+  },
+  {
+    "revision": "f4ac746ef89302c4bdec",
+    "url": "/react-dashboard/static/js/runtime-main.962c6d9a.js"
+  }
+]);
